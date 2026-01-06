6251420b5acdf0ffb9d9fa7f35317500ef5986f3
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-06 20:07:07 +0600

    practice with methods

--- lib/methods/example_method.dart
+++ lib/methods/example_method.dart
@@ -1,5 +1,5 @@
 void main() {
-  // фильтрация
+  // фильтрация where
   var allProducts = ["Плитка", "Панель", "Гибкий мрамор", "Пластик"];
   var onlyTiles = allProducts.where((item) => item.contains('Плитка'));
   var onlyPanel = allProducts.where((item) => item == "Панель").toList();
@@ -8,9 +8,50 @@ void main() {
 
   print("-------------");
 
+  // конвйер - (оброботка) map
+
   var numbers = [100, 200, 300];
 
   final result = numbers.map((item) => item * 20).toList();
 
   print(result);
+
+  print("-------------");
+
+  // поисковая - собака
+
+  var products = [
+    {"id": 1, "name": "Плинтус"},
+    {"id": 2, "name": "Гибкий мрамор"},
+    {"id": 3, "name": "Потолочный пластик"},
+  ];
+
+  final marble = products.firstWhere((item) => item['id'] == 2);
+
+  print(marble);
+
+  //  поиск индекса элемента по условию
+
+  final a = [10, 20, 30, 40, 50];
+
+  int searchIndex = a.indexWhere((item) => item == 30);
+  print(searchIndex);
+
+  print("-------------");
+
+  // replaceAll - меняет существующий элемент на месте
+
+  // String replaceAll(Pattern from, String replace)
+
+  String text = "Привет, мир! Мир прекрасен!";
+
+  String welcome = "Привет мир мир";
+
+  final resultWelcome = welcome.replaceAll("мир", "Dart");
+
+  final chagneText =
+      text.replaceAll("Привет, мир! Мир прекрасен!", "Здравствуйте, Вселенная");
+
+  print(chagneText);
+  print(resultWelcome);
 }
