3369e0c56c9b530d20f19a33235e0f84ed002f62
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-07 20:32:18 +0600

    test(class): initial object for ProductBuildingMaterials

--- lib/oop/main_class.dart
+++ lib/oop/main_class.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'package:dart_app/oop/class_example.dart';
+
+void main() {
+  ProductBuildingMaterials tile =
+      ProductBuildingMaterials('Арча', 'для пол мроамарный', 45, 7);
+}
